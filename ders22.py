2d5ba76b7900fec1a1a1c1160fc72a35efde1125
erhan3861 <artpencil34@gmail.com>
2023-11-25 16:05:59 +0300

    ders21 and ders22

--- ders22.py
+++ ders22.py
@@ -1,5 +1,6 @@
 from ursina import *
 from ursina.shaders import basic_lighting_shader as bls
+import json
 
 Entity.default_shader = bls
 
@@ -14,6 +15,9 @@ def input(key):
         else:
             anim.resume()
 
+def update():
+    pass    
+
 app = Ursina(borderless=False)
 
 anim = FrameAnimation3d(name="anim_copter\\anim_", fps=60, texture="copter_baked", autoplay=False, rotation_y = 180)
@@ -24,5 +28,18 @@ ground = Entity(model="plane", scale=300, texture="shore")
 Sky()
 
 cam = EditorCamera()
+
+with open("helicopter_position.json", "r") as file:
+    loc_dict = json.load(file)
+
+point = Entity(model="sphere", scale=3, color=color.red)
+
+point.sequence = Sequence(loop=True, auto_destroy=False)
+for i in loc_dict:
+    point.sequence.append(Func(setattr, point, 'position', tuple(loc_dict[i])))
+    point.sequence.append(Wait(1/30))
+point.sequence.start()
+
+sf = cam.add_script(SmoothFollow(target=point, offset=(0,0.5,-4)))
 
 app.run()
